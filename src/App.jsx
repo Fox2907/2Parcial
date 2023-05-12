46501027616255a198a99676c7a5a17c4ca013db
JFoxK29 <jfkontiveros@hotmail.com>
2023-05-12 11:01:25 -0400

    primer componente

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ButtonAdd from './components/ButtonAdd'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <ButtonAdd/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ButtonAdd.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ButtonAdd.css'
+
+const ButtonAdd = () => {
+  return (
+    <div className='button-add' >
+        <button>Add</button> 
+    </div>
+  )
+}
+
+export default ButtonAdd
